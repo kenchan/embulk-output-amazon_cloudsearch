0c4c5ee5391fcb163982de2f5cfb3d860d07cb12
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-05-10 12:57:42 +0900

    implement batch upload

--- lib/embulk/output/amazon_cloudsearch.rb
+++ lib/embulk/output/amazon_cloudsearch.rb
@@ -14,7 +14,8 @@ module Embulk
           'endpoint' => config.param('endpoint', :string),
           'id_column' => config.param('id_column', :string),
           'upload_columns' => config.param('upload_columns', :array),
-          'stub_response' => config.param('stub_response', :bool, default: false)
+          'stub_response' => config.param('stub_response', :bool, default: false),
+          'batch_size' => config.param('batch_size', :integer, default: 1000)
         }
 
         # resumable output:
@@ -38,6 +39,7 @@ module Embulk
         @id_column = task['id_column']
         @upload_columns = task['upload_columns']
         @stub_response = task['stub_response']
+        @batch_size = task['batch_size']
       end
 
       def close
@@ -45,26 +47,29 @@ module Embulk
 
       def add(page)
         # output code:
-        page.each do |record|
-          hash = Hash[schema.names.zip(record)]
+        page.each_slice(@batch_size) do |records|
+          documents = records.map do |record|
+            hash = Hash[schema.names.zip(record)]
+
+            {
+              type: "add",
+              id: hash[@id_column].to_s,
+              fields: @upload_columns.inject({}) {|acc, c| acc[c] = hash[c]; acc }
+            }
+          end
 
           res = client.upload_documents(
-            documents: [
-              {
-                type: "add",
-                id: hash[@id_column].to_s,
-                fields: @upload_columns.inject({}) {|acc, c| acc[c] = hash[c]; acc }
-              }
-            ].to_json,
+            documents: documents.to_json,
             content_type: 'application/json'
           )
+
           Embulk.logger.debug { "embulk-output-amazon_cloudsearch: response #{res}" }
 
           unless res.status == 'success'
-            Embulk.logger.error { "embulk-output-amazon_cloudsearch: data #{hash}, response #{res}" }
+            Embulk.logger.error { "embulk-output-amazon_cloudsearch: id #{documents.first[:id]}-#{documents.last[:id]}, response #{res}" }
           end
           if res.warnings
-            Embulk.logger.warn { "embulk-output-amazon_cloudsearch: response #{res}" }
+            Embulk.logger.warn { "embulk-output-amazon_cloudsearch: id #{documents.first[:id]}-#{documents.last[:id]}, response #{res}" }
           end
         end
       end
